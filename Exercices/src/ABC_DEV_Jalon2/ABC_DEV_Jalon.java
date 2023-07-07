4684999542f8b21b1fb0dd0b1920132f68009be4
EddieNamite <137295189+EddieNamite@users.noreply.github.com>
2023-07-07 10:08:26 +0200

    ABCDEV Jalon2

---
+++ Exercices/src/ABC_DEV_Jalon2/ABC_DEV_Jalon.java
@@ -0,0 +1,45 @@
+package ABC_DEV_Jalon2;
+
+import java.util.Scanner;
+
+public class ABC_DEV_Jalon {
+
+	public static void main(String[] args) {
+
+	boolean etudiant;
+	String jour;
+	boolean mercredi;
+	boolean jeudi;
+
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Etes vous etudiant ?");
+	etudiant = sc.next();
+	
+	if(etudiant.equals("O") || etudiant.equals("o"))
+	{
+		etudiant = true;
+	}
+	else
+	{
+		etudiant = false;
+	}
+	
+	System.out.println("Quel jour de la semaine sommes nous ?");
+	jour = sc.next();
+	
+	if(jour = "mardi")
+	{
+		
+	}
+	
+	
+	
+	
+	
+	}
+
+}
+
+// Je pense avoir à peu près réussi l'organigramme et le pseudo code mais pour le code JAVA j' ai trop stressé et j'ai tout mélangé.......
