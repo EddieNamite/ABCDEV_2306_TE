4b68003ed311e7d5fc1d88672fea1cfa88dccec6
EddieNamite <137295189+EddieNamite@users.noreply.github.com>
2023-07-31 15:41:30 +0200

    Fonctions_Procedures

--- Exercices/src/Exercice3_1/App.java
+++ Exercices/src/Exercice3_1/App.java
@@ -1,10 +1,61 @@
 package Exercice3_1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Déclaration des variables
+		
+				int nombre;
+				int sup;
+				int essai;
+				int min;
+				int max;
+				boolean win;
+			
+				Scanner scanner = new Scanner(System.in);
+				Random random = new Random();
+				
+				nombre = random.nextInt(100)+1;
+				
+				essai = 0 ;
+				win = false;
+				min = 0;
+				max = 100;
+				
+				System.out.println("Saisir un nombre entier entre 0 et 100"); 
+				
+				do {
+							
+					sup = scanner.nextInt();
+
+					if (nombre != sup)  {			
+						
+						if (sup > nombre) {
+							System.out.println("Entrer un nombre compris entre : "+sup+" et "+min);
+							max = sup;
+										}
+						else {
+							System.out.println("Entrer un nombre compris entre : "+max+" et "+sup);
+							min = sup;
+							}		
+						}
+					
+						if (nombre == sup) {
+							win = true;}			
+						
+						essai = essai+1; 
+								
+					}while (win != true);
+						
+					System.out.println(" Bravo ! reussi en "+essai+" essais");
+
+				scanner.close();
+				
+				}
 
 	}
 
-}
+
